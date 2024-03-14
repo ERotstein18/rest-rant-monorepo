f0c8338d76ac3097754823fb18446f615e296d9a
Eyrma <erotstein@outlook,com>
2024-03-13 21:30:36 -0700

    changes for role-based auth

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -6,15 +6,22 @@ function CurrentUserProvider({ children }) {
   const [currentUser, setCurrentUser] = useState(null);
 
   useEffect(() => {
+
     const getLoggedInUser = async () => {
-      let response = await fetch(
-        `http://localhost:5000/authentication/profile`,
-        {
-          credentials: 'include',
+      try{
+        let response = await fetch('http://localhost:5000/authentication/profile', {
+            credentials: 'include'
+        })
+        if (response.status === 401) {
+          setCurrentUser(null);
+          return;
         }
-      );
-      let user = await response.json();
-      setCurrentUser(user);
+
+        let user = await response.json();
+        setCurrentUser(user);
+      } catch (error) {
+        console.error('Error fetching user:', error);
+      }
     };
     getLoggedInUser();
   }, []);
